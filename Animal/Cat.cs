4879d428fc357ee21f1dc8a9e4cda782c8b396f1
eddieandersson68 <eddieandersson68@users.noreply.github.com>
2017-10-13 19:42:07 +0200

    Add files via upload

---
+++ Animal/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    public class Cat
+    {
+        
+        public void Description()
+        {
+            
+            Console.WriteLine("I'm a cat and I'm 4 years old");
+        }
+    }
+}
