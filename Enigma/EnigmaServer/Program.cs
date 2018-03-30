9f65b32b934815de0c463254d9f9c07e347bbdde
Eddie Andersson <eddie.andersson68@gmail.com>
2018-03-30 16:29:28 +0200

    Woked on the GUI some and added MVVM folders.

---
+++ Enigma/EnigmaServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace EnigmaServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(8888);
+            int RequestCount = 0;
+
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(">> Server started");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine(">> client connection accepted");
+            RequestCount = 0;
+
+            while (true)
+            {
+                try
+                {
+                    RequestCount += + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesFrom = new byte[10025];
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> Data from client - " + dataFromClient);
+                    string serverResponse = "Last Message from client" + dataFromClient;
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    
+                }
+            }
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> exit");
+            Console.ReadLine();
+
+           
+        }
+    }
+}
